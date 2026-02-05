fa653c47f50f304636d65afb6d8a3ef640e208af
Wesley Siqueira <seu_email_do_github@gmail.com>
2026-02-05 12:27:19 -0400

    feat: adicionando grafico de importancia das variaveis

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score as acc
@@ -80,3 +81,12 @@ importancia = pd.DataFrame({
 }).sort_values(by='Importancia', ascending=False)
 
 print(importancia)
+
+plt.figure(figsize=(10,6))
+plt.barh(importancia['coluna name'], importancia['Importancia'], color='skyblue')
+plt.xlabel('Importância (%)')
+
+plt.title('Fatores que mais pesam na Decisão de Crédito (XGBoost)')
+plt.gca().invert_yaxis()
+plt.tight_layout()
+plt.show()
